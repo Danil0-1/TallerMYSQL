d60fe148d6ae71d8754f67580f00ea6026fce012
Danil0-1 <danilomuskus1@gmail.com>
2025-06-04 11:00:23 -0500

    feat: :mag: 1. Encuentra los nombres de los clientes y los nombres de los empleados que gestionaron sus pedidos 2.

---
+++ ConsultasMultitablaJoins.sql
@@ -0,0 +1,7 @@
+-- Active: 1748981230048@@127.0.0.1@3307@Taller
+
+SELECT usuarios.nombre, detalles_pedidos.pedido_id, detalles_pedidos.producto_id 
+FROM usuarios
+JOIN pedidos ON pedidos.cliente_id = usuarios.usuario_id
+JOIN detalles_pedidos ON detalles_pedidos.pedido_id = pedidos.pedido_id;
+
