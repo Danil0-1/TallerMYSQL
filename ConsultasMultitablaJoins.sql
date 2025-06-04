4d81d372424164e25c3f762513c2780d22159412
Danil0-1 <danilomuskus1@gmail.com>
2025-06-04 17:34:08 -0500

    feat: :mag: Muestra todos los pedidos y, si existen, los productos en cada pedido, incluyendo los pedidos sin productos usando `LEFT JOIN`

--- ConsultasMultitablaJoins.sql
+++ ConsultasMultitablaJoins.sql
@@ -17,3 +17,16 @@ INNER JOIN usuarios AS c ON p.cliente_id = c.usuario_id
 INNER JOIN empleados emp ON p.empleado_id = emp.empleado_id
 INNER JOIN usuarios AS e ON emp.usuario_id = e.usuario_id
 GROUP BY p.pedido_id;
+
+SELECT 
+    productos.nombre AS Producto,
+    pedidos.cliente_id AS Cliente_id,
+    pedidos.empleado_id AS Empleado_id,
+    detalles_pedidos.cantidad AS Cantidad,
+    pedidos.fecha_pedido AS Fecha,
+    pedidos.estado AS Estado
+FROM pedidos
+LEFT JOIN detalles_pedidos
+ON pedidos.pedido_id = detalles_pedidos.pedido_id
+LEFT JOIN productos
+ON detalles_pedidos.producto_id = productos.producto_id;
