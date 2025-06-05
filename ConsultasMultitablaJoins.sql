71eac9af72baf62e72b9e2589fd6fd72a2667ad4
Danil0-1 <danilomuskus1@gmail.com>
2025-06-05 06:47:44 -0500

    feat: :mag: Listar todos los proveedores que suministran un determinado producto.

--- ConsultasMultitablaJoins.sql
+++ ConsultasMultitablaJoins.sql
@@ -73,3 +73,15 @@ SELECT
 FROM usuarios
 LEFT JOIN pedidos
 ON usuarios.usuario_id = pedidos.cliente_id;
+
+SELECT
+    proveedores.nombre AS Proveedor,
+    productos.nombre AS Producto,
+    productos.categoria AS Categoria,
+    productos.precio AS Precio
+FROM proveedores
+JOIN proveedores_productos
+    ON proveedores.proveedor_id = proveedores_productos.proveedor_id
+JOIN productos
+    ON proveedores_productos.producto_id = productos.producto_id
+WHERE productos.nombre = 'Laptop';
