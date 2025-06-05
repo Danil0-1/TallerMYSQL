53975da0e6c8301f153bab2eda463f90b6ee5a2d
Danil0-1 <danilomuskus1@gmail.com>
2025-06-05 06:52:07 -0500

    feat: :mag: Cuenta cuántos productos suministra cada proveedor, mostrando `proveedor_id`, `nombre_proveedor` y `total_productos`.

--- ConsultasMultitablaJoins.sql
+++ ConsultasMultitablaJoins.sql
@@ -127,3 +127,12 @@ FROM proveedores
 JOIN proveedores_productos
     ON proveedores.proveedor_id = proveedores_productos.proveedor_id
 WHERE proveedores_productos.producto_id = 1;
+
+SELECT 
+    productos.producto_id,
+    productos.nombre AS Producto,
+    COUNT(proveedores_productos.proveedor_id) AS cantidad_proveedores
+FROM productos
+JOIN proveedores_productos
+    ON productos.producto_id = proveedores_productos.producto_id
+GROUP BY productos.producto_id, productos.nombre;
