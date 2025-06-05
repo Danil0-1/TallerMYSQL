1555554bf977aeb5f5f777d0ef15d1c6c47745a1
Danil0-1 <danilomuskus1@gmail.com>
2025-06-05 06:48:16 -0500

    feat: :mag: Obtener todos los productos que ofrece un proveedor específico.

--- ConsultasMultitablaJoins.sql
+++ ConsultasMultitablaJoins.sql
@@ -85,3 +85,15 @@ JOIN proveedores_productos
 JOIN productos
     ON proveedores_productos.producto_id = productos.producto_id
 WHERE productos.nombre = 'Laptop';
+
+SELECT
+    proveedores.nombre AS Proveedor,
+    productos.nombre AS Producto,
+    productos.categoria AS Categoria,
+    productos.precio AS Precio
+FROM proveedores
+JOIN proveedores_productos
+    ON proveedores.proveedor_id = proveedores_productos.proveedor_id
+JOIN productos
+    ON proveedores_productos.producto_id = productos.producto_id
+WHERE proveedores.nombre = 'Tech Supplies S.A.'; 
